ae8b4f2edeb218acbc5416ed729ef081214f0950
Nur Alam <nuralam1122345@gmail.com>
2025-09-28 10:45:36 +0600

    login section add

--- script/login.js
+++ script/login.js
@@ -1,7 +1,16 @@
-
-document.getElementById('login-btn')
- .addEventListener('click',
- function(event){
-    event.preventDefault()
-
-} )
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const inputNumber = document.getElementById("input-number").value;
+    const inputPin = document.getElementById("input-pin").value;
+    if (inputNumber.length === 11) {
+      if (inputPin === "1234") {
+        window.location.href = "../html-index/main.html";
+      } else {
+        alert("input valid pin");
+      }
+    } else {
+      alert("input valid number");
+    }
+  });
