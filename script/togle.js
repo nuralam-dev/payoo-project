6dbb196fb7c42fcf93b98896b57044095fce8caa
Nur Alam <nuralam1122345@gmail.com>
2025-10-01 13:01:55 +0600

    cash out section

---
+++ script/togle.js
@@ -0,0 +1,12 @@
+document.getElementById("cash-out").style.display = "none";
+document.getElementById("add-money").style.display = "none";
+
+document.getElementById("add-money-btn").addEventListener("click", function () {
+  document.getElementById("add-money").style.display = "block";
+  document.getElementById("cash-out").style.display = "none";
+});
+
+document.getElementById("cash-out-btn").addEventListener("click", function () {
+  document.getElementById("cash-out").style.display = "block";
+  document.getElementById("add-money").style.display = "none";
+});
